42d596c4a85f49192f4cf8ea6bba61067b588832
Antonioquiros <antonioquigar18@gmail.com>
2025-02-13 20:09:04 +0100

    cambio en json

--- Servicio1/servicio1.py
+++ Servicio1/servicio1.py
@@ -1,12 +1,12 @@
 from flask import Flask, jsonify
-import requests
+import json
 
 app1 = Flask(__name__)
 
 @app1.route('/<int:municipioid>/geo', methods=['GET'])
 def get_geo(municipioid):
-    url = f"https://www.el-tiempo.net/api/json/v2/provincias/23/municipios{municipioid}"
-    response = requests.get(url)
+    url = f"https://www.el-tiempo.net/api/json/v2/provincias/23/municipios/{municipioid}"
+    response = json.get(url)
 
     if response.status_code == 200:
         return jsonify(response.json()), 200
